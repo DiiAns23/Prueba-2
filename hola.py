fcc929deeaf528605b1d52e36738a283d0100c43
Diego Andrés Obín Rosales <diego.obin23@gmail.com>
2020-12-07 18:27:38 -0600

    Crear Funcion Adios
    Se creo la funcion que dice Adios

--- hola.py
+++ hola.py
@@ -5,3 +5,8 @@ print("Linea 4 editada por Diego")
 def funcion_que_dice_hola():
     print("Hola")
 funcion_que_dice_hola()
+
+def funcion_que_dice_adios():
+    print("Adios banda me voy")
+
+funcion_que_dice_adios()
